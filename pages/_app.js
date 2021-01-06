e058b304b0de73040d3e71b01f39b4c53df0d986
M Afzaal Afzal <afzaal1721@gmail.com>
2021-01-06 20:52:10 +0500

    "a bug fixed"

--- pages/_app.js
+++ pages/_app.js
@@ -26,7 +26,7 @@ export default function MyApp(props) {
                 <meta charSet='utf-8'/>
                 <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
                 <meta name='viewport'
-                      content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'/>
+                      content='width=device-width,initial-scale=1'/>
                 <meta name='description' content='An Ecommerce website from where the users can buy different things according to their choice. Developed by M Afzaal Afzal'/>
                 <meta name='keywords' content='E-Commerce Web App by M Afzaal Afzal'/>
                 <link rel="manifest" href="/manifest.json"/>
